de26652b3aa0912af3ef3e6648ff25d582cd1a56
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-14 00:10:54 +0300

    Add interface Idbservice and implement this by dbimplementation

--- api.Implementation/DbImplementation.cs
+++ api.Implementation/DbImplementation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using api.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace api.Implementation
 {
-    public partial class DbImplementation
+    public partial class DbImplementation : IDbService
     {
         private SqlConnection GetSqlConnection()
         {
---
+++ api.Interfaces/IDbService.cs
@@ -0,0 +1,14 @@
+﻿using api.Types.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Interfaces
+{
+    public interface IDbService
+    {
+        GetMoviesResponse GetMovies();
+    }
+}
