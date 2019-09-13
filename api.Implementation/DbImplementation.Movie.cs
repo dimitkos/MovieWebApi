b9a062ba5272a96798bfe370e654421c14deeb6d
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-14 00:07:49 +0300

    Implement GetMovies end point

--- api.Implementation/DbImplementation.Movie.cs
+++ api.Implementation/DbImplementation.Movie.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using api.Types.DbTypes;
+using api.Types.Responses;
+using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,18 @@ namespace api.Implementation
 {
     public partial class DbImplementation
     {
+        public GetMoviesResponse GetMovies()
+        {
+            string sql = @"Select * From movie with no lock";
+            using (var con = GetSqlConnection())
+            {
+                var response = con.Query<Movie>(sql);
 
+                return new GetMoviesResponse()
+                {
+                    Movies = response
+                };
+            }
+        }
     }
 }
