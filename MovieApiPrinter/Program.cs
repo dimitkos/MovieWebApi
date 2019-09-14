0291527d500fce28e289cd0c204c8420ee9a9ef1
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-14 20:26:03 +0300

    create method that call the end point movie/getMovies

--- MovieApiPrinter/Program.cs
+++ MovieApiPrinter/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using api.Types.Responses;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,25 @@ namespace MovieApiPrinter
     {
         static void Main(string[] args)
         {
+
+            GetMoviesAndPrint();
+
+            Console.ReadLine();
+        }
+
+        public static void GetMoviesAndPrint()
+        {
+            var client = new RestClient("http://localhost:59881");
+            var request = new RestRequest("api/movie/getMovies", Method.GET);
+            var queryResult = client.Get<GetMoviesResponse>(request).Data;
+
+            foreach(var movie in queryResult.Movies)
+            {
+                Console.WriteLine("Title: " + movie.Title);
+                Console.WriteLine("Name: " + movie.Name);
+                Console.WriteLine("Description " + movie.Description);
+                Console.WriteLine("-----------------------------------------------------------------");
+            }
         }
     }
 }
