c0ffa4a0d8cf69dc1da40f3a0543ed6905fe192b
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-15 12:03:29 +0300

    Add class SetProject which dispalys info in console and take the imput from the user

--- MovieApiPrinter/Program.cs
+++ MovieApiPrinter/Program.cs
@@ -1,4 +1,5 @@
-﻿using api.Types.Responses;
+﻿using api.Types.Requests;
+using api.Types.Responses;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,12 @@ namespace MovieApiPrinter
     {
         static void Main(string[] args)
         {
-
-            GetMoviesAndPrint();
-
-            Console.ReadLine();
+            //PostMovie();
+            //GetMoviesAndPrint();
+            SetProject st = new SetProject();
+            st.StartUpProject();
+            //StartUpProject();
+            //Console.ReadLine();
         }
 
         public static void GetMoviesAndPrint()
@@ -30,6 +33,92 @@ namespace MovieApiPrinter
                 Console.WriteLine("Name: " + movie.Name);
                 Console.WriteLine("Description " + movie.Description);
                 Console.WriteLine("-----------------------------------------------------------------");
+            }
+        }
+
+        public static void PostMovie()
+        {
+            Console.WriteLine("Please Enter the Title");
+            string title = Console.ReadLine();
+            Console.WriteLine("Please Enter the Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please Enter the Description");
+            string description = Console.ReadLine();
+
+            AddMovieRequest request = new AddMovieRequest()
+            {
+                Title = title,
+                Name = name,
+                Description = description
+            };
+
+            var client = new RestClient("http://localhost:59881");
+            var postRequest = new RestRequest("api/movie/addMovie", Method.POST);
+            postRequest.AddParameter("application/json; charset=utf-8", ParameterType.RequestBody);
+            postRequest.RequestFormat = DataFormat.Json;
+        }
+
+        public static void StartUpProject()
+        {
+            while(true)
+            {
+                Console.WriteLine("Please Choose Action");
+                Console.WriteLine("[1] Get Movies");
+                Console.WriteLine("[2] Add Movie");
+                Console.WriteLine("[3] Update Movie");
+                Console.WriteLine("[4] Delete Movie");
+
+                string input = Console.ReadLine();
+
+                if(input=="1")
+                {
+                    //call the get
+                }
+                else if(input == "2")
+                {
+                    //Console.WriteLine("Please Enter the Title");
+                    //string title = Console.ReadLine();
+                    //Console.WriteLine("Please Enter the Name");
+                    //string name = Console.ReadLine();
+                    //Console.WriteLine("Please Enter the Description");
+                    //string description = Console.ReadLine();
+
+                    //AddMovieRequest request = new AddMovieRequest()
+                    //{
+                    //    Title = title,
+                    //    Name = name,
+                    //    Description = description
+                    //};
+
+                    //var client = new RestClient("http://localhost:59881");
+                    //var postRequest = new RestRequest("api/movie/getMovies", Method.POST);
+                    //postRequest.AddParameter("application/json; charset=utf-8", ParameterType.RequestBody);
+                    //postRequest.RequestFormat = DataFormat.Json;
+                }
+                else if(input == "3")
+                {
+                    //update
+                }
+                else if(input == "4")
+                {
+                    //delete
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input");
+                }
+
+                Console.WriteLine("Would you like more actions?? [Y] or [N]");
+                var answer = Console.ReadLine();
+                if(answer == "Y" || answer == "y")
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
             }
         }
     }
---
+++ MovieApiPrinter/SetProject.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApiPrinter
+{
+    public class SetProject
+    {
+        public void StartUpProject()
+        {
+            while (true)
+            {
+                DisplayInfo();
+
+                //var input = UserInput();
+
+                CheckInput(UserInput());
+
+                if (ContinueOrNot())
+                {
+                    Console.Clear();
+                    continue;
+                }
+                break;
+            }
+        }
+
+        #region private methods
+
+        private void DisplayInfo()
+        {
+            Console.WriteLine("Please Choose Action");
+            Console.WriteLine("[1] Get Movies");
+            Console.WriteLine("[2] Add Movie");
+            Console.WriteLine("[3] Update Movie");
+            Console.WriteLine("[4] Delete Movie");
+        }
+
+        private string UserInput()
+        {
+            string input = Console.ReadLine();
+            return input.Trim();
+        }
+
+        private void CheckInput(string input)
+        {
+            if (input == "1")
+            {
+                //call the get
+            }
+            else if (input == "2")
+            {
+                //Console.WriteLine("Please Enter the Title");
+                //string title = Console.ReadLine();
+                //Console.WriteLine("Please Enter the Name");
+                //string name = Console.ReadLine();
+                //Console.WriteLine("Please Enter the Description");
+                //string description = Console.ReadLine();
+
+                //AddMovieRequest request = new AddMovieRequest()
+                //{
+                //    Title = title,
+                //    Name = name,
+                //    Description = description
+                //};
+
+                //var client = new RestClient("http://localhost:59881");
+                //var postRequest = new RestRequest("api/movie/getMovies", Method.POST);
+                //postRequest.AddParameter("application/json; charset=utf-8", ParameterType.RequestBody);
+                //postRequest.RequestFormat = DataFormat.Json;
+            }
+            else if (input == "3")
+            {
+                //update
+            }
+            else if (input == "4")
+            {
+                //delete
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+        }
+
+        private bool ContinueOrNot()
+        {
+            Console.WriteLine("Would you like more actions?? [Y] or [N]");
+
+            if (UserInput().ToLower() == "y")
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
+        }
+
+        #endregion
+    }
+}
